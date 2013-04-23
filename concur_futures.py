f41b270ca3e65be94bf20e6741345a1ec3a25863
Chris McDevitt <christophermcdevitt@gmail.com>
2013-04-23 17:30:11 +0100

    better

--- concur_futures.py
+++ concur_futures.py
@@ -1,6 +1,7 @@
 import futures
 from multiprocessing import Queue
-
+import operator
+import functools
 
 if __name__ == '__main__':
     numq = Queue()
@@ -11,12 +12,13 @@ if __name__ == '__main__':
         z = numq.get()
         stringchan.put("%d + %d + %d = %d" % (x, y, z, x + y + z))
 
-    def putnum(x):
-        numq.put(x)
+    def putnum(f):
+        numq.put(f())
     with futures.ProcessPoolExecutor() as executor:
         stringfuture = executor.submit(add_nums)
-        executor.submit(putnum, 2 * 10)
-        executor.submit(putnum, 2 * 20)
-        executor.submit(putnum, 30 + 40)
+        executor.map(putnum,
+                     [functools.partial(operator.mul, 2, 10),
+                      functools.partial(operator.mul, 2, 20),
+                      functools.partial(operator.add, 30, 40)])
 
-    print stringchan.get()
+    print stringchan.get(timeout=5)
