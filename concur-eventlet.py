4ee30f787e6948ef9e8aabf46724b4fcad0f9a8a
Edward George <edwardgeorge@gmail.com>
2013-04-24 22:51:32 +0100

    remove unneeded lambdas

--- concur-eventlet.py
+++ concur-eventlet.py
@@ -11,8 +11,8 @@ if __name__ == '__main__':
         z = numbers.get()
         return "%d + %d + %d = %d" % (x, y, z, x + y + z)
 
-    eventlet.spawn(lambda: numbers.put(2 * 10))
-    eventlet.spawn(lambda: numbers.put(2 * 20))
-    eventlet.spawn(lambda: numbers.put(30 + 40))
+    eventlet.spawn(numbers.put, 2 * 10)
+    eventlet.spawn(numbers.put, 2 * 20)
+    eventlet.spawn(numbers.put, 30 + 40)
 
     print proc.wait()
