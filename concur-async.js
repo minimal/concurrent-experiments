8c4081d2c08edbb4f9d57fa7dfb1e48316d14996
Stuart Quin <stuart.quin@gmail.com>
2013-04-24 17:36:05 +0100

    Add node version

---
+++ concur-async.js
@@ -0,0 +1,24 @@
+async = require( "async" );
+
+async.parallel([
+    function(cb){
+        cb(null, 2 * 10);
+    },
+    function(cb){
+        cb(null, 2 * 20);
+    },
+    function(cb){
+        cb(null, 30 + 40);
+    }
+],
+// Callback to display
+function(err, results){
+    var answer = 0;
+    var result = "";
+    for( var i in results ){
+        result += results[i]+"+";
+        answer += results[i];
+    }
+    result = result.substring(0, result.length-1);
+    console.log( result + " = " + answer );
+});
