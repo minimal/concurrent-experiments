66feff9542ec5c8ad39a173597287597fddb7a3c
Edward George <edwardgeorge@gmail.com>
2013-04-24 22:55:48 +0100

    no real need for the __name__ == '__main__'

--- concur-eventlet.py
+++ concur-eventlet.py
@@ -1,18 +1,17 @@
 import eventlet
 
+numbers = eventlet.queue.Queue()
 
-if __name__ == '__main__':
-    numbers = eventlet.queue.Queue()
 
-    @eventlet.spawn
-    def proc():
-        x = numbers.get()
-        y = numbers.get()
-        z = numbers.get()
-        return "%d + %d + %d = %d" % (x, y, z, x + y + z)
+@eventlet.spawn
+def proc():
+    x = numbers.get()
+    y = numbers.get()
+    z = numbers.get()
+    return "%d + %d + %d = %d" % (x, y, z, x + y + z)
 
-    eventlet.spawn(numbers.put, 2 * 10)
-    eventlet.spawn(numbers.put, 2 * 20)
-    eventlet.spawn(numbers.put, 30 + 40)
+eventlet.spawn(numbers.put, 2 * 10)
+eventlet.spawn(numbers.put, 2 * 20)
+eventlet.spawn(numbers.put, 30 + 40)
 
-    print proc.wait()
+print proc.wait()
