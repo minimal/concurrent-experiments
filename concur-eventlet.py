2925232074eff16d74a826c65a61828bd6e7e44e
Edward George <edwardgeorge@gmail.com>
2013-04-25 14:50:31 +0100

    do work in thread itself

--- concur-eventlet.py
+++ concur-eventlet.py
@@ -1,3 +1,5 @@
+# eventlet runs the spawned processes in coroutines (greenlets) in a single thread
+# so we don't see speed up here.
 import eventlet
 
 numbers = eventlet.queue.Queue()
@@ -10,8 +12,8 @@ def proc():
     z = numbers.get()
     return "%d + %d + %d = %d" % (x, y, z, x + y + z)
 
-eventlet.spawn(numbers.put, 2 * 10)
-eventlet.spawn(numbers.put, 2 * 20)
-eventlet.spawn(numbers.put, 30 + 40)
+eventlet.spawn(lambda: numbers.put(2 * 10))
+eventlet.spawn(lambda: numbers.put(2 * 20))
+eventlet.spawn(lambda: numbers.put(30 + 40))
 
 print proc.wait()
