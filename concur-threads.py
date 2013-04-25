18655ca4f494076f7eeb89359ee36a7741c0e73f
Edward George <edwardgeorge@gmail.com>
2013-04-25 14:52:10 +0100

    do work in thread itself

--- concur-threads.py
+++ concur-threads.py
@@ -13,7 +13,7 @@ def proc():
     result.put("%d + %d + %d = %d" % (x, y, z, x + y + z))
 
 threading.Thread(target=proc).start()
-threading.Thread(target=numbers.put, args=(2 * 10, )).start()
-threading.Thread(target=numbers.put, args=(2 * 20, )).start()
-threading.Thread(target=numbers.put, args=(30 + 40, )).start()
+threading.Thread(target=lambda: numbers.put(2 * 10)).start()
+threading.Thread(target=lambda: numbers.put(2 * 20)).start()
+threading.Thread(target=lambda: numbers.put(30 + 40)).start()
 print result.get()
