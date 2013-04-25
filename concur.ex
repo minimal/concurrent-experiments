bc367bf27b0885319b0700c6c3adf859ec0d2b28
Chris McDevitt <christophermcdevitt@gmail.com>
2013-04-25 19:36:54 +0100

    simplify elixir

--- concur.ex
+++ concur.ex
@@ -1,31 +1,26 @@
 current_pid = self
 
 receiver = spawn fn ->
-receive do
-    { :result, res } ->
-    a = res
     receive do
-        {:result, res2 } ->
-        b = res2
+        {a} ->
         receive do
-            {:result, res3 } ->
-            c = res3
-            d = a + b + c
-            current_pid <- {:final, "#{a} + #{b} + #{c} = #{d}"}
+            {b} ->
+            receive do
+                {c} ->
+                d = a + b + c
+                current_pid <- {"#{a} + #{b} + #{c} = #{d}"}
+            end
         end
     end
 end
-end
 
-
-spawn fn -> receiver <- {:result, 2 * 10}
+spawn fn ->receiver <- { 2 * 10}
 end
-spawn fn -> receiver <- {:result, 2 * 20}
+spawn fn -> receiver <- { 2 * 20}
 end
-spawn fn -> receiver <- {:result, 30 + 40}
+spawn fn -> receiver <- { 30 + 40}
 end
 
 receive do
-    {:final, result} ->
-    IO.puts result
+    {result} -> IO.puts result
 end
