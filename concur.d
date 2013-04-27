36eac2c1deca3545500758dfcb9411677ddb00f6
Richard Johnson <richard@skimlinks.com>
2013-04-27 18:18:07 +0100

    D implementation.
    Apparently, you used to be able to do something like:
    
        core.thread.spawn( (){ tid.send(2 * 10); } )
    
    Which would have been nicer than function pointers, but it appears this
    has been removed.

---
+++ concur.d
@@ -0,0 +1,27 @@
+import std.stdio, std.concurrency, std.c.stdio, std.format, std.array;
+ 
+void recieverFunc(Tid tid)
+{
+    int a, b, c = 0;
+    receive( (int msg) { a = msg; } );
+    receive( (int msg) { b = msg; } );
+    receive( (int msg) { c = msg; } );
+
+    auto writer = appender!string();
+    formattedWrite(writer, "%d + %d + %d = %d", a, b, c, a + b + c);
+    send(tid, writer.data);
+}
+
+void add(Tid tid, int x, int y) { tid.send(x + y); }
+void mul(Tid tid, int x, int y) { tid.send(x * y); }
+ 
+void main()
+{
+    auto tid = std.concurrency.spawn(&recieverFunc, thisTid);
+    spawn(&mul, tid, 2, 10);
+    spawn(&mul, tid, 2, 20);
+    spawn(&add, tid, 30, 40);
+ 
+    auto response = receiveOnly!(string);
+    writeln(response);
+}
