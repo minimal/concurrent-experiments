11fc0960b75f33b797975e19836397d88b90acbb
Chris McDevitt <christophermcdevitt@gmail.com>
2013-04-27 22:00:35 +0100

    no need for nested receives

--- concur.ex
+++ concur.ex
@@ -1,26 +1,21 @@
 current_pid = self
-
-receiver = spawn fn ->
+receiver = spawn fn -> 
     receive do
-        {a} ->
-        receive do
-            {b} ->
-            receive do
-                {c} ->
-                d = a + b + c
-                current_pid <- {"#{a} + #{b} + #{c} = #{d}"}
-            end
-        end
+        {a} -> 
+    end
+    receive do
+        {b} -> 
+    end
+    receive do
+        {c} -> 
+        d = a + b + c
+        current_pid <- {"#{a} + #{b} + #{c} = #{d}"}
     end
 end
 
-spawn fn ->receiver <- { 2 * 10}
-end
-spawn fn -> receiver <- { 2 * 20}
-end
-spawn fn -> receiver <- { 30 + 40}
-end
-
+spawn fn -> receiver <- { 2 * 10 } end
+spawn fn -> receiver <- { 2 * 20 } end
+spawn fn -> receiver <- { 30 + 40 } end
 receive do
     {result} -> IO.puts result
 end
