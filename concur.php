cfbf856090dc44b10be573a495b64453dcd901b0
Richard Johnson <richard@skimlinks.com>
2013-04-27 15:47:31 +0100

    Correct formatting in PHP version

--- concur.php
+++ concur.php
@@ -7,20 +7,20 @@ class storage extends Stackable {
 class myAsync extends Thread {
     public function __construct($storage, $num1, $num2, $operation=NULL) {
         $this->storage = $storage;
-		$this->num1 = $num1;
-		$this->num2 = $num2;
-		$this->op = $operation;
+        $this->num1 = $num1;
+        $this->num2 = $num2;
+        $this->op = $operation;
     }
 
     public function run(){
-	  switch ($this->op){
-        case 'add':
-		  $this->storage[] = $res = $this->num1 + $this->num2;
-		break;
-        default:
-          $this->storage[] = $res = $this->num1 * $this->num2;
-        break;
-      }
+        switch ($this->op){
+            case 'add':
+                $this->storage[] = $res = $this->num1 + $this->num2;
+                break;
+            default:
+                $this->storage[] = $res = $this->num1 * $this->num2;
+                break;
+        }
     } 
 }
 
@@ -36,25 +36,15 @@ $myAsync3 = new myAsync($storage, 30, 40, "add");
 $myAsync3->start();
 
 if ($myAsync1->join() && $myAsync2->join() && $myAsync3->join()){
-	
-	$result = 0;
-	$string = "";
 
-	foreach($storage as $value){
-		$result += $value;
-		$string .= $value . " + ";
-	}
+    $result = 0;
+    $string = "";
 
-	print substr($string, 0, -3) . " = ".$result;
+    foreach($storage as $value){
+        $result += $value;
+        $string .= $value . " + ";
+    }
+
+    print substr($string, 0, -3) . " = ".$result;
 }
 
-
-
-
-
-
-
-
-
-  
-?>
