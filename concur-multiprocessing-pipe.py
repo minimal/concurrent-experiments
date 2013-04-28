5ca9129cde9e9c0393da0b21fbae567cb6a108ef
Edward George <edwardgeorge@gmail.com>
2013-04-28 22:04:18 +0100

    python: updated multiprocessing pipe example to have write to share pipe wrapped in lock

--- concur-multiprocessing-pipe.py
+++ concur-multiprocessing-pipe.py
@@ -1,5 +1,6 @@
 import multiprocessing
 
+
 def proc(conn):
     x = conn.recv()
     y = conn.recv()
@@ -7,10 +8,16 @@ def proc(conn):
     conn.send("%d + %d + %d = %d" % (x, y, z, x + y + z))
     conn.close()
 
+
+def withlock(lock, call, *args, **kw):
+    with lock:
+        return call(*args, **kw)
+
 if __name__ == '__main__':
+    lock = multiprocessing.RLock()
     parent_conn, child_conn = multiprocessing.Pipe()
     multiprocessing.Process(target=proc, args=(child_conn,)).start()
-    multiprocessing.Process(target=lambda: parent_conn.send(2 * 10)).start()
-    multiprocessing.Process(target=lambda: parent_conn.send(2 * 20)).start()
-    multiprocessing.Process(target=lambda: parent_conn.send(30 + 40)).start()
+    multiprocessing.Process(target=lambda: withlock(lock, parent_conn.send, 2 * 10)).start()
+    multiprocessing.Process(target=lambda: withlock(lock, parent_conn.send, 2 * 20)).start()
+    multiprocessing.Process(target=lambda: withlock(lock, parent_conn.send, 30 + 40)).start()
     print parent_conn.recv()
