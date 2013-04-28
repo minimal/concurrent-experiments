21da24fa7671afe1246bd2940f6950098f84ff2c
Edward George <edwardgeorge@gmail.com>
2013-04-28 22:20:57 +0100

    expanded comment

--- singlecore/concur-eventlet.py
+++ singlecore/concur-eventlet.py
@@ -1,5 +1,8 @@
 # eventlet runs the spawned processes in coroutines (greenlets) in a single thread
-# so we don't see speed up here.
+# so we don't see speed up here. eventlet spawned coroutines behave pretty-much
+# *exactly* how you'd expect similar goroutines to run in go WHEN GOMAXPROCS is 1.
+# ie: only one runs at any one time and only yields to others when making a blocking
+# call or explicitly yielding. greenlets cannot be multiplexed over multiple cores.
 import eventlet
 
 numbers = eventlet.queue.Queue()
