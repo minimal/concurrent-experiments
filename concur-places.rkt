607e15e4c3f57de304158aea493201aa5e563a9c
Edward George <edwardgeorge@gmail.com>
2013-05-01 22:58:04 +0100

    put/get at same time

--- concur-places.rkt
+++ concur-places.rkt
@@ -6,9 +6,8 @@
               (define-values (x y z) (apply values (build-list 3 (lambda (i) (place-channel-get in)))))
               (place-channel-put ch (format "~a + ~a + ~a = ~a" x y z (+ x y z)))))
   (define-values (numbers-put numbers-get) (place-channel))
-  (place-channel-put p numbers-get)
   (map (lambda (pl) (place-channel-put pl numbers-put)) (list
       (place ch (define out (place-channel-get ch)) (place-channel-put out (* 2 10)))
       (place ch (define out (place-channel-get ch)) (place-channel-put out (* 2 20)))
       (place ch (define out (place-channel-get ch)) (place-channel-put out (+ 30 40)))))
-  (printf (place-channel-get p)) (newline))
+  (printf (place-channel-put/get p numbers-get)) (newline))
