6670aff80321468707315c0222977484867956d9
Edward George <edwardgeorge@gmail.com>
2013-05-23 00:25:31 +0100

    added haskell

---
+++ concur.hs
@@ -0,0 +1,17 @@
+import Control.Concurrent
+import Control.Concurrent.Chan
+import Text.Printf
+
+main = do
+        numbers <- newChan :: IO (Chan Int)
+        result <- newChan :: IO (Chan String)
+        forkIO $ do
+            x <- readChan numbers
+            y <- readChan numbers
+            z <- readChan numbers
+            writeChan result (printf "%d + %d + %d = %d" x y z (x + y + z))
+        forkIO $ do { writeChan numbers (2 * 10) }
+        forkIO $ do { writeChan numbers (2 * 20) }
+        forkIO $ do { writeChan numbers (30 + 40) }
+        resultStr <- readChan result
+        putStrLn resultStr
