eb69c8d6121d969c49acab1cfab455e94dd8a0f1
Chris McDevitt <christophermcdevitt@gmail.com>
2013-08-17 17:29:30 +0100

    Create clojurescript.core.async.cljs
    using core.async go and channels. Works in browser

---
+++ clojurescript.core.async.cljs
@@ -0,0 +1,15 @@
+(defn concur []
+  (let [numchan (chan)
+        stringchan (chan)]
+    (go
+     (let [a (<! numchan)
+           b (<! numchan)
+           c (<! numchan)]
+       (>! stringchan (str a " + " b " + " c " = " (+ a b c)))))
+    (go (<! (timeout (rand-int 100))) (>! numchan (* 2 10)))
+    (go (<! (timeout (rand-int 100))) (>! numchan (* 2 20)))
+    (go (<! (timeout (rand-int 100))) (>! numchan (+ 30 40)))
+ 
+    (go (println  (<! stringchan)))))
+ 
+(concur)
