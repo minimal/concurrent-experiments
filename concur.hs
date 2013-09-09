9c46d04e677e69c25949833ebf49d3a6912e3f8b
Edward George <edwardgeorge@gmail.com>
2013-09-09 23:59:21 +0100

    remove parenthesis un-needed with dollar operator

--- concur.hs
+++ concur.hs
@@ -9,7 +9,7 @@ main = do
             x <- readChan numbers
             y <- readChan numbers
             z <- readChan numbers
-            writeChan result $! (printf "%d + %d + %d = %d" x y z (x + y + z))
+            writeChan result $! printf "%d + %d + %d = %d" x y z (x + y + z)
         forkIO $ do writeChan numbers $! (2 * 10)
         forkIO $ do writeChan numbers $! (2 * 20)
         forkIO $ do writeChan numbers $! (30 + 40)
