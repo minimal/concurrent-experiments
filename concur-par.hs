b7ec3838ccb99470100d8d72a0ab0ffe35df30ff
Edward George <edwardgeorge@gmail.com>
2014-05-31 12:25:17 +0100

    pattern match all@

--- concur-par.hs
+++ concur-par.hs
@@ -5,8 +5,8 @@ spawnProcs :: Par String
 spawnProcs = do
     [a, b, c] <- sequence [new, new, new]
     d <- spawn $ do
-        [x, y, z] <- sequence [get a, get b, get c]
-        return $ printf "%d + %d + %d = %d" x y z (x + y + z)
+        all@[x, y, z] <- sequence [get a, get b, get c]
+        return $ printf "%d + %d + %d = %d" x y z (sum all)
     fork $ put a ((2 * 10) :: Int)
     fork $ put b ((2 * 20) :: Int)
     fork $ put c ((30 + 40) :: Int)
