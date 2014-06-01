838fbfc7172e5a3663edb4840fdef6927cabe695
Edward George <edwardgeorge@gmail.com>
2014-06-01 16:56:43 +0100

    added concurrent pipes

---
+++ ConcurPipes.hs
@@ -0,0 +1,24 @@
+module ConcurPipes where
+import Control.Concurrent (forkIO)
+import Control.Monad (replicateM, mapM_)
+import Pipes
+import Pipes.Concurrent
+import Text.Printf
+
+proc :: Pipe Int String IO ()
+proc = do
+  all@[x, y, z] <- replicateM 3 await
+  yield $ printf "%d + %d + %d = %d" x y z (sum all)
+
+main :: IO ()
+main = do
+  (output, input) <- spawn Unbounded
+  (output', input') <- spawn Single
+  mapM_ (run output) [x, y, z]
+  run output' $ fromInput input >-> proc
+  runEffect $ for (fromInput input') (lift . putStrLn)
+  where x = yield (2 * 10)
+        y = yield (2 * 20)
+        z = yield (30 + 40)
+        run o f = forkIO $ do runEffect $ f >-> toOutput o
+                              performGC
