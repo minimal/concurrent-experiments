f146577f175496b2b14e0430d5a8bfb087068118
Edward George <edwardgeorge@gmail.com>
2014-06-01 17:38:57 +0100

    moved where to let

--- ConcurPipes.hs
+++ ConcurPipes.hs
@@ -12,13 +12,14 @@ proc = do
 
 main :: IO ()
 main = do
+  let x = yield (2 * 10)
+      y = yield (2 * 20)
+      z = yield (30 + 40)
+      run o f = forkIO $ do
+                  runEffect $ f >-> toOutput o
+                  performGC
   (output, input) <- spawn Unbounded
   (output', input') <- spawn Single
   mapM_ (run output) [x, y, z]
   run output' $ fromInput input >-> proc
   runEffect $ for (fromInput input') (lift . putStrLn)
-  where x = yield (2 * 10)
-        y = yield (2 * 20)
-        z = yield (30 + 40)
-        run o f = forkIO $ do runEffect $ f >-> toOutput o
-                              performGC
