11af48652073cba08b4d0ba7e966267e99bed305
Edward George <edwardgeorge@gmail.com>
2014-06-03 22:14:26 +0100

    added comments showing how to build a channel

--- concur.jl
+++ concur.jl
@@ -1,4 +1,7 @@
 # julia coroutines example
+# the below uses the built-in scheduler and yielding
+# in an emulation of an actor implementation.
+
 proc = Task(() -> begin
   x = wait()
   y = wait()
@@ -9,4 +12,21 @@ end)
 @schedule(yieldto(proc, 2 * 20))
 @schedule(yieldto(proc, 30 + 40))
 println(consume(proc))
-           
+
+# alternatively, we could easily build a queue for
+# communication between the coroutines to be similar
+# to some of the other implementations:
+#
+# type Queue
+#   cond::Condition
+#   values::Array{Any, 1}
+#   Queue() = new(Condition(), Any[])
+# end
+# put(q::Queue, v) = (push!(q.values, v); notify(q.cond))
+# get(q::Queue) = while true
+#   if length(q.values) > 0
+#      return shift!(q.values)
+#   else
+#     wait(q.cond)
+#   end
+# end
