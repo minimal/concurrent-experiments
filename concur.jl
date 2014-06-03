b24da02ae6e41373f03f82cb357acb1d1ca21fde
Edward George <edwardgeorge@gmail.com>
2014-06-03 21:29:18 +0100

    no need to schedule proc, can use produce/consume

--- concur.jl
+++ concur.jl
@@ -1,16 +1,11 @@
-function proc(main)
-  t = Task(() -> begin
-    x = wait()
-    y = wait()
-    z = wait()
-    yieldto(main, "$x + $y + $z = $(x + y + z)")
-  end)
-  schedule(t)
-  t
-end
-p = proc(current_task())
-@schedule(yieldto(p, 2 * 10))
-@schedule(yieldto(p, 2 * 20))
-@schedule(yieldto(p, 30 + 40))
-println(wait())
+proc = Task(() -> begin
+  x = wait()
+  y = wait()
+  z = wait()
+  produce("$x + $y + $z = $(x + y + z)")
+end)
+@schedule(yieldto(proc, 2 * 10))
+@schedule(yieldto(proc, 2 * 20))
+@schedule(yieldto(proc, 30 + 40))
+println(consume(proc))
            
