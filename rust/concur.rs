24f9db63ce05261e33e5ce0a42192eca063148ef
Chris McDevitt <christophermcdevitt@gmail.com>
2014-08-11 16:02:40 +0100

    rust implementation to rust 0.11.0

--- rust/concur.rs
+++ rust/concur.rs
@@ -1,23 +1,21 @@
-use core::task::spawn;
-use core::comm::{stream, Port, Chan, SharedChan};
+use std::task::spawn;
 
 fn main() {
-    let (out, in): (Port<int>, Chan<int>) = stream();
-    let in = SharedChan(in);
-    let (strout, strin): (Port<~str>, Chan<~str>) = stream();
+    let (tx, rx) = channel::<int>();
+    let (strtx, strrx) =  channel();
 
-    do spawn || {
-        let x = out.recv();
-        let y = out.recv();
-        let z = out.recv();
-        strin.send(fmt!("%d + %d + %d = %d" , x , y , z , x + y + z));
-    }
+    spawn(proc() {
+        let x = rx.recv();
+        let y = rx.recv();
+        let z = rx.recv();
+        strtx.send(format!("{} + {} + {} = {}" , x , y , z , x + y + z));
+    });
 
-    let my_in = in.clone();
-    do spawn || { my_in.send(2 * 10); }
-    let my_in = in.clone();
-    do spawn || { my_in.send(2 * 20); }
-    let my_in = in.clone();
-    do spawn || { my_in.send(30 + 40); }
-    io::println(strout.recv());
+    let my_in = tx.clone();
+    spawn(proc() { my_in.send(2 * 10); });
+    let my_in = tx.clone();
+    spawn(proc() { my_in.send(2 * 20); });
+    let my_in = tx.clone();
+    spawn(proc() { my_in.send(30 + 40); });
+    println!("{}", strrx.recv());
 }
