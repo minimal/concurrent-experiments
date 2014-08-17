b38d9796b519b87d615606c68b3385d607ccf22b
Edward George <edwardgeorge@gmail.com>
2014-08-17 19:39:19 +0100

    added applicative instance example to async as comments

--- haskell/ConcurAsync.hs
+++ haskell/ConcurAsync.hs
@@ -10,3 +10,25 @@ main = do
               all@[x, y, z] <- mapConcurrently wait numbers
               return $ printf "%d + %d + %d = %d" x y z (sum all)
   wait result >>= putStrLn
+
+-- Alternatively, for this example, we could use the applicative
+-- instance of the Concurrently newtype wrapper:
+
+-- > import Control.Applicative
+-- > import Control.Concurrent.Async
+-- > import Text.Printf
+-- >
+-- > format :: Int -> Int -> Int -> String
+-- > format x y z = printf "%d + %d + %d = %d" x y z (x + y + z)
+-- >
+-- > main :: IO ()
+-- > main = do
+-- >   resp <- runConcurrently $ pure format
+-- >        <*> Concurrently (return $ 2 * 10)
+-- >        <*> Concurrently (return $ 2 * 20)
+-- >        <*> Concurrently (return $ 30 + 40)
+-- >   putStrLn resp
+
+-- Intentionally use `pure` on the applied `format` function
+-- rather than fmap `format <$> ...` so that it gets its own
+-- thread of execution.
