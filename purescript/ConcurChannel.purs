5a5cf1f741e92214cd6657875b06207bb1d164ea
Edward George <edwardgeorge@gmail.com>
2014-09-15 18:42:00 +0100

    Move channel impl into purescript from js using foreign for existential

--- purescript/ConcurChannel.purs
+++ purescript/ConcurChannel.purs
@@ -4,44 +4,6 @@ import Control.Monad.Eff
 import Control.Monad.Eff.Ref
 import Control.Monad.Cont.Trans
 import Control.Monad.Trans
-import Data.Maybe
-
-foreign import data Async :: !
-foreign import data Channel :: * -> *
-
-foreign import newChan
-  "function newChan() {\
-  \    return { data: [],\
-  \             waiters: [] };\
-  \}" :: forall a r. Eff (async :: Async | r) (Channel a)
-
-foreign import readChan
-  "function readChan(chan) {\
-  \  return function(cont) {\
-  \    return function() {\
-  \      if (chan.data.length > 0) {\
-  \         cont(chan.data.shift())();\
-  \        }\
-  \      else {\
-  \        chan.waiters.push(cont); }\
-  \      return {};\
-  \    }\
-  \  }\
-  \}" :: forall a eff. Channel a -> ContT Unit (Eff eff) a
-
-foreign import writeChan
-  "function writeChan(chan) {\
-  \  return function(val) {\
-  \   return function() {\
-  \     if(chan.waiters.length > 0) {\
-  \         chan.waiters.shift()(val)();\
-  \       }\
-  \     else {\
-  \       chan.data.push(val); }\
-  \     return {};\
-  \   }\
-  \  }\
-  \}" :: forall a r. Channel a -> a -> Eff (async :: Async | r) Unit
 
 foreign import setTimeout
   "function setTimeout(t) {\
@@ -54,12 +16,71 @@ foreign import setTimeout
   \  }\
   \}" :: forall eff eff' a. Number -> Eff eff a -> Eff eff' Unit
 
-proc :: forall eff. Channel Number -> Channel String -> ContT Unit (Eff (async :: Async | eff)) Unit
+data NonEmptyList a = Singleton a | NEList a (NonEmptyList a)
+
+appendNE :: forall a. NonEmptyList a -> a -> NonEmptyList a
+appendNE (Singleton a) b = NEList a (Singleton b)
+appendNE (NEList a b) c = NEList a (appendNE b c)
+
+foreign import data Async :: !
+foreign import data Callback :: * -> *
+foreign import newCallback
+  "function newCallback(fn){\
+  \  return {\"fn\": fn};\
+  \}" :: forall a eff. (a -> Eff eff Unit) -> Callback a
+foreign import runCallback
+  "function runCallback(cb) {\
+  \  return function(val) {\
+  \    return function() {\
+  \      return cb.fn(val)();\
+  \    }\
+  \  }\
+  \}" :: forall a eff. Callback a -> a -> Eff (async :: Async | eff) Unit
+
+data ChannelContent a = EmptyChan
+                    | Waiters (NonEmptyList (Callback a))
+                    | Values (NonEmptyList a)
+
+newtype Channel a = Channel (RefVal (ChannelContent a))
+
+readChan (Channel a) = ContT $ \f -> do
+  x <- readRef a
+  case x of
+    EmptyChan -> writeRef a (Waiters (Singleton (newCallback f)))
+    Waiters b -> writeRef a (Waiters (appendNE b (newCallback f)))
+    Values (Singleton b) -> do
+                             writeRef a EmptyChan
+                             f b
+    Values (NEList b c) -> do
+                            writeRef a (Values c)
+                            f b
+
+writeChan (Channel a) v = do
+  x <- readRef a
+  case x of
+    EmptyChan -> writeRef a (Values (Singleton v))
+    Values b -> writeRef a (Values (appendNE b v))
+    Waiters (Singleton b) -> do
+                              writeRef a EmptyChan
+                              runCallback b v
+    Waiters (NEList b c) -> do
+                             writeRef a (Waiters c)
+                             runCallback b v
+
+newChan :: forall a eff. Eff (ref :: Ref | eff) (Channel a)
+newChan = do
+  c <- newRef EmptyChan
+  return $ Channel c
+
+proc :: forall eff. Channel Number -> Channel String
+        -> ContT Unit (Eff (ref :: Ref, async :: Async | eff)) Unit
 proc num res = do
   x <- readChan num
   y <- readChan num
   z <- readChan num
-  lift $ writeChan res $ show x ++ " + " ++ show y ++ " + " ++ show z ++ " = " ++ show (x + y + z)
+  let sum = x + y + z
+      f = show x ++ " + " ++ show y ++ " + " ++ show z ++ " = " ++ show sum
+  lift $ writeChan res f
 
 main = do
   c <- newChan
