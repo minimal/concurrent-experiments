479cbdbc7eec8abd7dd1d1643632c07eb9776e7b
Edward George <edwardgeorge@gmail.com>
2014-11-27 20:50:11 +0000

    example added using new purescript-parallel library

---
+++ purescript/ConcurParallel.purs
@@ -0,0 +1,32 @@
+module ConcurParallel where
+import Control.Monad.Eff
+import Control.Monad.Cont.Trans
+import Control.Parallel
+import Debug.Trace
+
+foreign import data Timeout :: *
+
+foreign import setTimeoutImpl
+  "function setTimeoutImpl(t) {\
+  \  var env = typeof global !== 'undefined' ? global : window;\
+  \  return function(fn) {\
+  \    return function() {\
+  \      return env.setTimeout(function(){fn({})();}, t);\
+  \    }\
+  \  }\
+  \}" :: forall eff a. Number -> (Unit -> Eff eff a) -> Eff eff Timeout
+
+async :: forall eff a. (Unit -> Eff eff a) -> ContT Unit (Eff eff) a
+async f = ContT $ \x -> let f' _ = f unit >>= x -- purescript isn't lazy!
+                         in void $ setTimeoutImpl 0 f'
+
+format :: Number -> Number -> Number -> String
+format a b c = show a ++ " + " ++ show b ++ " + " ++ show c
+                      ++ " = " ++ show (a + b + c)
+
+main = run concur trace
+     where a = Parallel <<< async $ \_ -> return (2 * 10)
+           b = Parallel <<< async $ \_ -> return (2 * 20)
+           c = Parallel <<< async $ \_ -> return (30 + 40)
+           concur = format <$> a <*> b <*> c
+           run = runContT <<< runParallel
