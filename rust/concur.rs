c48870ba5b89036531d23640267c06f22ba2aeec
Chris McDevitt <christophermcdevitt@gmail.com>
2015-03-28 14:37:05 +0000

    unwrap sends so quite warnings

--- rust/concur.rs
+++ rust/concur.rs
@@ -9,15 +9,15 @@ fn main() {
         let x = rx.recv().unwrap();
         let y = rx.recv().unwrap();
         let z = rx.recv().unwrap();
-        strtx.send(format!("{} + {} + {} = {}" , x , y , z , x + y + z));
+        strtx.send(format!("{} + {} + {} = {}" , x , y , z , x + y + z)).unwrap();
     });
 
     let my_in = tx.clone();
-    spawn(move || { my_in.send(2 * 10); });
+    spawn(move || { my_in.send(2 * 10).unwrap() });
     let my_in = tx.clone();
-    spawn(move || { my_in.send(2 * 20); });
+    spawn(move || { my_in.send(2 * 20).unwrap() });
     let my_in = tx.clone();
-    spawn(move || { my_in.send(30 + 40); });
+    spawn(move || { my_in.send(30 + 40).unwrap() });
     match strrx.recv() {
         Ok(v)  => println!("{}", v),
         Err(e) => println!("Error receiving {:?}", e),
